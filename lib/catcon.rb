17887d244abf33e94ea9507e64383d8d4532f858
Joshua Hawxwell <m@hawx.me>
2012-03-12 18:59:25 +0000

    Added debug option and exceptions

--- lib/catcon.rb
+++ lib/catcon.rb
@@ -23,9 +23,44 @@ require 'catcon/parser'
 require 'catcon/interpreter'
 
 module Catcon
+  extend self
 
-  def self.new(opts={})
+  def colour(str, code)
+    "\e[#{code}m#{str}\e[0m"
+  end
+
+  class Error < ::Exception
+    def initialize(msg=nil, backtrace=[], stack=[])
+      @backtrace = backtrace
+      msg += "\n    " + Catcon.colour("Stack: ", 90) + stack.inspect
+      super(msg)
+    end
+
+    def backtrace
+      @backtrace.map {|i| Catcon.colour("Trace: ", 90) + i }
+    end
+  end
+
+  # When there isn't enough on the stack raise this!
+  class InsufficientStackError < Error; end
+  # When a stack contains the wrong values for the function called
+  class IncorrectTypeError < Error; end
+  # When a function is called which has not been defined
+  class UndefinedError < Error; end
+
+
+  def new(opts={})
+    @opts = opts
     Interpreter.new(opts)
+  end
+
+  def debug(str, level=nil)
+    m = {
+      :error   => 1, # red
+      :warn    => 3, # yellow
+      :success => 2  # green
+    }
+    puts "\e[#{m[level]}m#{str}\e0m" if @opts[:debug]
   end
 
 end
